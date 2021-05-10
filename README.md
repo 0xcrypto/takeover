3108a79e3ff726d0e657e57c7043c80ca78b026f
0xcrypto <vi@hackberry.xyz>
2021-05-10 16:11:25 +0530

    need to pip

---
+++ README.md
@@ -0,0 +1,57 @@
+# takeover.py
+This small script tries to detect subdomain takeovers from a list of domains. Fingerprints are taken from https://github.com/EdOverflow/can-i-take-over-xyz.
+
+
+## Installation
+
+```
+pip install takeover
+```
+
+## Usage
+
+```
+takeover blog.example.com
+```
+
+Using with other tools:
+
+```
+subfinder -d "example.com" | takeover
+```
+
+Using in python:
+
+```python
+import json, asyncio
+from pathlib import Path
+from takeover import takeover
+
+home = str(Path.home())
+
+# config is an dictionary. See ~/.config/takeover/config.json for structure
+config = json.load(open(home + "/.config/takeover/config.json"))
+
+# Do not forget to replace pointer to fingerprints with the valid data. See ~/.config/takeover/fingerprints.json for structure
+config['fingerprints'] = json.load(open(home + "/.config/takeover/fingerprints.json"))
+
+subdomains = ["blog.example.com", "services.example.com"]
+
+asyncio.run(takeover(config).checkHost(subdomains))
+```
+
+## How it Works
+* Matches CNAME against takeover-able services
+* If CNAME found, matches fingerprints in the body.
+
+## Note
+* As I use discord a lot, this script is programmed to notify using discord webhooks. So you will need to have a discord server and create a webhook to use in it. 
+* If you need some extra features, feel free to submit a new issue on GitHub.
+
+## License
+[LICENSE.md](LICENSE.md)
+
+## Disclaimer
+I make guns, I sell guns, I give away guns but I take no responsibility of who dies with the guns.
+
+_Legally speaking, What you do with this has nothing to do with me. I am not responsible for your actions._
---
+++ takeover.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import asyncio, requests, sys, os, dns.resolver, json
+from discord import Webhook, RequestsWebhookAdapter
+from urllib.parse import urlparse
+from dns.resolver import Resolver, NXDOMAIN, NoNameservers, Timeout, NoAnswer
+from pathlib import Path
+
+home = str(Path.home())
+
+class takeover:
+    def __init__(self, configuration):
+        self.inform = Webhook.from_url(configuration['discord_webhook'], adapter=RequestsWebhookAdapter())
+        self.fingerprints = configuration['fingerprints']
+
+    async def subDomainTakeOver(self, domain, cname, fingerprint):
+        for rdata in cname:
+            for cname in fingerprint['cname']:
+                if(cname and cname in str(rdata.target)):
+                    website = 'http://' + domain
+                    request = requests.get(website)
+                    if(fingerprint['fingerprint'] in request.text):
+                        print("[+] %s matched for domain %s" % (fingerprint['service'], domain))
+                        self.inform.send("""
+                            Hey%s,\n%s is %s to subdomain takeover on %s. Fingerprint is `%s`
+                        """ % ('<@' + config['user_id'] + '>' if(config['user_id']) else "", website, fingerprint['status'].lower().strip(), fingerprint['service'], fingerprint['fingerprint']))
+  
+    async def checkHosts(self, args=[]):
+        recheck = []
+        try:
+            if(not len(args)):
+                args = sys.argv
+                args.pop(0)
+            if(not len(args)):
+                print("Reading from PIPE... Raise KeyboardInterrupt to stop it.")
+                args = list(set(sys.stdin))
+
+            for domain in args:
+                validdomain = domain
+                if not(domain.startswith('http://') or domain.startswith('https://')):
+                    validdomain = (urlparse('http://' + domain).netloc).strip()
+                try:
+                    cname = dns.resolver.query(validdomain, 'CNAME')
+                    [await self.subDomainTakeOver(validdomain, cname, fingerprint) for fingerprint in self.fingerprints]
+
+                except NoNameservers:
+                    print("[x] DNS No No nameservers: %s" % validdomain)
+                except Timeout:
+                    recheck.append(validdomain)
+                    print("[x] DNS Timeout: %s"  % validdomain)
+                except NoAnswer:
+                    print("[x] DNS No Answer for CNAME: %s"  % validdomain)
+                except NXDOMAIN:
+                    print("[x] DNS NXDOMAIN: %s"  % validdomain)
+
+            if(len(recheck)):
+                await self.checkHosts(recheck)
+
+        except IndexError:
+            print("[x] No argument provided!")
+            exit(1)
+        except KeyboardInterrupt:
+            print("[x] KeyboardInterrupt occurred.")
+            exit(1)
+
+
+if __name__ == '__main__':
+    try:
+        config = json.load(open(home + "/.config/takeover/config.json"))
+        config['fingerprints'] = json.load(open(home + "/.config/takeover/fingerprints.json"))
+        config['user_id'] = config['user_id'] if 'user_id' in config else ""
+    except FileNotFoundError:
+        if('yes' == input("Takeover is not configured! To continue, you need to configure it. (type yes to continue): ")):
+            config = {
+                "fingerprints": input("Enter Path/URL for fingerprints.json. Leave blank to use default: ") or "https://gist.githubusercontent.com/0xcrypto/c0344960476193d8af7dbb310cc04958/raw/06ed5533702fc56b2e126eb600f8f8ce2967961d/sdto.json",
+                "discord_webhook": input("Enter Discord webhook. Leave blank if you do not wish to use discord: "),
+                "user_id": input("Enter your Discord User ID to get mentioned in the notification. Leave blank for no mention: ")
+            }
+            print("Your configuration is: ")
+            print(json.dumps(config, sort_keys=True, indent=4))
+
+            if(config['fingerprints'].startswith('http://') or config['fingerprints'].startswith('https://')):
+                fingerprints = json.loads(requests.get(config['fingerprints']).text)
+            else:
+                fingerprints = json.load(open(config['fingerprints']))
+
+            if('yes' == input("Would you like to save this? (yes to save the configuration, leave blank to use it temporarily): ")):
+                if not os.path.exists(home + '/.config/takeover'):
+                    os.makedirs(home + "/.config/takeover")
+               
+                with open(home + "/.config/takeover/fingerprints.json", 'w') as fingerprintFile:
+                    json.dump(fingerprints, fingerprintFile)
+
+                with open(home + "/.config/takeover/config.json", 'w') as configFile:
+                    json.dump(config, configFile)
+
+                print("Configuration file updated successfully!")
+
+            config = {
+                "discord_webhook": config['discord_webhook'],
+                "fingerprints": fingerprints,
+                "user_id": config['user_id'],
+            }
+        else:
+            print("Without fingerprints, takeover cannot detect subdomain takeovers!")
+            exit(1)
+
+    asyncio.run(takeover(config).checkHosts())
